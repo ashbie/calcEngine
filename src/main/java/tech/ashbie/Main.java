11e51e5da7947a8c4efab25d59553cc781b4331c
Ashbie <ashbie.b.mweemba@gmail.com>
2024-11-12 16:36:42 +0100

    use loops and arrays

--- src/main/java/tech/ashbie/Main.java
+++ src/main/java/tech/ashbie/Main.java
@@ -4,10 +4,15 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("\n\n\tThis is going to be a Calculator Engine");
 
-        double value1 = 100.0d;
-        double value2 = 0.0d;
-        double result;
-        char opCode = 'd';
+        double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
+        double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
+        char[] opCodes = {'d', 'a', 's', 'm'};
+        double[] results = new double[opCodes.length];
+
+//        double value1 = 100.0d;
+//        double value2 = 0.0d;
+//        double result;
+//        char opCode = 'd';
 
         // version if
         // if(opCode == 'a') {
@@ -34,25 +39,31 @@ public class Main {
         // }
 
         // version switch
-        switch(opCode) {
-            case 'a':
-                result = value1 + value2;
-                break;
-            case 's':
-                result = value1 - value2;
-                break;
-            case 'm':
-                result = value1 * value2;
-                break;
-            case 'd':
-                result = (value2 != 0) ? (value1 / value2) : 0.0d;
-                break;
-            default:
-                System.out.println("\tInvalid opCode(operation code): " + opCode);
-                result = 0.0d;
-            break;
+        for(int i = 0; i < opCodes.length; i++) {
+            switch(opCodes[i]) { // <-- opCode sub i
+                case 'a':
+                    results[i] = leftVals[i] + rightVals[i];
+                    break;
+                case 's':
+                    results[i] = leftVals[i] - rightVals[i];
+                    break;
+                case 'm':
+                    results[i] = leftVals[i] * rightVals[i];
+                    break;
+                case 'd':
+                    results[i] = (rightVals[i] != 0) ? (leftVals[i] / rightVals[i]) : 0.0d;
+                    break;
+                default:
+                    System.out.println("\tInvalid opCode(operation code): " + opCodes[i]);
+                    results[i] = 0.0d;
+                    break;
+            }
         }
 
-        System.out.println("\tResult = " + result + "\n");
+
+
+        for(double res : results) {
+            System.out.println("\tResult = " + res + "\n");
+        }
     }
 }
