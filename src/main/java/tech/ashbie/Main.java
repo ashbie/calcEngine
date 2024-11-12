e373cbf81e017627b4830ef93a6c1e1062ea7df4
Ashbie <ashbie.b.mweemba@gmail.com>
2024-11-12 10:26:15 +0100

    switch from if-version to switch-version

--- src/main/java/tech/ashbie/Main.java
+++ src/main/java/tech/ashbie/Main.java
@@ -2,6 +2,57 @@ package tech.ashbie;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("\n\nThis is going to be a Calculator Engine");
+        System.out.println("\n\n\tThis is going to be a Calculator Engine");
+
+        double value1 = 100.0d;
+        double value2 = 0.0d;
+        double result;
+        char opCode = 'd';
+
+        // version if
+        // if(opCode == 'a') {
+        //     result = value1 + value2;
+        // }
+        // else if(opCode == 's') {
+        //     result = value1 - value2;
+        // }
+        // else if(opCode == 'm') {
+        //     result = value1 * value2;
+        // }
+        // else if(opCode == 'd') {
+        //     if(value2 != 0) {
+        //         result = value1 / value2;
+        //     }
+        //     else {
+        //         System.out.println("Error! Can't divide by 0!!!");
+        //         result = 0.0d;
+        //     }
+        // }
+        // else {
+        //     System.out.println("Invalid opCode(operation code): " + opCode);
+        //     result = 0.0d;
+        // }
+
+        // version switch
+        switch(opCode) {
+            case 'a':
+                result = value1 + value2;
+                break;
+            case 's':
+                result = value1 - value2;
+                break;
+            case 'm':
+                result = value1 * value2;
+                break;
+            case 'd':
+                result = (value2 != 0) ? (value1 / value2) : 0.0d;
+                break;
+            default:
+                System.out.println("\tInvalid opCode(operation code): " + opCode);
+                result = 0.0d;
+            break;
+        }
+
+        System.out.println("\tResult = " + result + "\n");
     }
 }
