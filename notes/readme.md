11981e45bfe6a839fa9c91a83ed4f38cc60049c5
Ashbie <ashbie.b.mweemba@gmail.com>
2024-11-12 19:08:37 +0100

    incorporate methods (an external method/function that will perform the math operations)

---
+++ notes/readme.md
@@ -0,0 +1,9 @@
+## Understanding Methods
+ - The return value of a method:
+         can use a variable
+         can use a literal or
+         can use a calculation.
+     WHAT IS a literal?
+     
+![returning an Array](image.png)
+![returning an Array](image-1.png)
--- src/main/java/tech/ashbie/Main.java
+++ src/main/java/tech/ashbie/Main.java
@@ -4,66 +4,42 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("\n\n\tThis is going to be a Calculator Engine");
 
-        double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
-        double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
-        char[] opCodes = {'d', 'a', 's', 'm'};
-        double[] results = new double[opCodes.length];
-
-//        double value1 = 100.0d;
-//        double value2 = 0.0d;
-//        double result;
-//        char opCode = 'd';
-
-        // version if
-        // if(opCode == 'a') {
-        //     result = value1 + value2;
-        // }
-        // else if(opCode == 's') {
-        //     result = value1 - value2;
-        // }
-        // else if(opCode == 'm') {
-        //     result = value1 * value2;
-        // }
-        // else if(opCode == 'd') {
-        //     if(value2 != 0) {
-        //         result = value1 / value2;
-        //     }
-        //     else {
-        //         System.out.println("Error! Can't divide by 0!!!");
-        //         result = 0.0d;
-        //     }
-        // }
-        // else {
-        //     System.out.println("Invalid opCode(operation code): " + opCode);
-        //     result = 0.0d;
-        // }
+        double[] leftVal = {100.0d, 25.0d, 225.0d, 11.0d};
+        double[] rightVal = {50.0d, 92.0d, 17.0d, 3.0d};
+        char[] opCode = {'d', 'a', 's', 'm'};
+        double[] result = new double[opCode.length];
 
         // version switch
-        for(int i = 0; i < opCodes.length; i++) {
-            switch(opCodes[i]) { // <-- opCode sub i
-                case 'a':
-                    results[i] = leftVals[i] + rightVals[i];
-                    break;
-                case 's':
-                    results[i] = leftVals[i] - rightVals[i];
-                    break;
-                case 'm':
-                    results[i] = leftVals[i] * rightVals[i];
-                    break;
-                case 'd':
-                    results[i] = (rightVals[i] != 0) ? (leftVals[i] / rightVals[i]) : 0.0d;
-                    break;
-                default:
-                    System.out.println("\tInvalid opCode(operation code): " + opCodes[i]);
-                    results[i] = 0.0d;
-                    break;
-            }
+        for(int i = 0; i < opCode.length; i++) {
+            result[i] = executeMathOperation(opCode[i], leftVal[i], rightVal[i]);
         }
 
-
-
-        for(double res : results) {
+        for(double res : result) {
             System.out.println("\tResult = " + res + "\n");
         }
+
+    }
+
+    static double executeMathOperation(char opCode, double leftVal, double rightVal) {
+        double result;
+        switch(opCode) { // <-- opCode sub i
+            case 'a':
+                result = leftVal + rightVal;
+                break;
+            case 's':
+                result = leftVal - rightVal;
+                break;
+            case 'm':
+                result = leftVal * rightVal;
+                break;
+            case 'd':
+                result = (rightVal != 0) ? (leftVal / rightVal) : 0.0d;
+                break;
+            default:
+                System.out.println("\tInvalid opCode(operation code): " + opCode);
+                result = 0.0d;
+                break;
+        }
+        return result;
     }
 }
